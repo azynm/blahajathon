517dfc41ea5b6e01c3267b45842a1020e7f0bb94
Aadi Deepchand <aadideepchand8254@gmail.com>
2026-03-14 17:58:05 +0000

    Cleaned login to discord

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session, request, render_template_string
+from flask import Flask, redirect, url_for, session, request, render_template
 import os
 import requests
 
@@ -11,14 +11,17 @@ DISCORD_AUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id={CLIENT_
 
 #Start Flask app
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.secret_key = os.urandom(24) #Remove this line to have persistency
+
+
 
 #Home screen
 @app.route('/')
 def index():
     #If not logged in, send to login page
     if 'access_token' not in session:
-        return render_template_string("login.html", auth_url=DISCORD_AUTH_URL, setup_done=False)
+        return render_template("login.html", auth_url=DISCORD_AUTH_URL)
 
     #Get all the servers the bot is in
     bot_servers = requests.get("https://discord.com/api/users/@me/guilds", headers={"Authorization": f"Bot {BOT_TOKEN}"}).json()
@@ -27,11 +30,39 @@ def index():
     #Get all the servers the user is in
     user_servers = requests.get("https://discord.com/api/users/@me/guilds", headers={"Authorization": f"Bearer {session['access_token']}"}).json()
     
+    #Make a list of all servers we can participate in
     servers = []
     for s in user_servers:
         if s["id"] in bot_server_ids:
+            s["bot_exists"] = True
             servers.append(s)
-        elif int(g['permissions']) & 0x20:
-            g['id'] in bot_guild_ids
-            managed_guilds.append(g)
-            
+        elif int(s['permissions']) & 0x20:
+            servers.append(s)
+    
+    #Show the page
+    return render_template("index.html", guilds=servers, username=session['username'], client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
+
+
+
+#Callback page
+@app.route('/callback')
+def callback():
+    #Get code from Discord callback for handshake
+    code = request.args.get('code')
+    
+    #Perform handshake
+    data = {'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET, 'grant_type': 'authorization_code', 'code': code, 'redirect_uri': REDIRECT_URI}
+    r = requests.post("https://discord.com/api/oauth2/token", data=data).json()
+    
+    #Get new access token
+    session['access_token'] = r.get('access_token')
+    
+    #Get username
+    user_data = requests.get("https://discord.com/api/users/@me", headers={"Authorization": f"Bearer {session['access_token']}"}).json()
+    session['username'] = user_data.get('username')
+    
+    return redirect(url_for('index'))
+
+#Actually starts web server
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
